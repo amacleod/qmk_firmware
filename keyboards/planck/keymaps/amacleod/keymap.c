642581d2188da5c5f4371873ae7914c0b90147ef
Allister MacLeod <allister.macleod@gmail.com>
2024-05-02 14:49:20 -0400

    Fix typo in KC_ASTR use on elevate layer

--- keyboards/planck/keymaps/amacleod/keymap.c
+++ keyboards/planck/keymaps/amacleod/keymap.c
@@ -164,10 +164,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ELEVATE] = LAYOUT_planck_grid(
-  DVORAK,  KC_F1, KC_F2,  KC_F3,   KC_F4,   KC_NO,   KC_SLSH, KC_AST, KC_DOT, KC_PLUS, KC_EQL,  KC_NO,
-  COLEMAK, KC_F5, KC_F6,  KC_F7,   KC_F8,   KC_NO,   KC_0,    KC_1,   KC_2,   KC_3,    KC_4,    KC_MINS,
-  QWERTY,  KC_F9, KC_F10, KC_F11,  KC_F12,  KC_NO,   KC_5,    KC_6,   KC_7,   KC_8,    KC_9,    KC_NO,
-  GAME_ON, KC_NO, KC_NO,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO
+  DVORAK,  KC_F1, KC_F2,  KC_F3,   KC_F4,   KC_NO,   KC_SLSH, KC_ASTR, KC_DOT, KC_PLUS, KC_EQL,  KC_NO,
+  COLEMAK, KC_F5, KC_F6,  KC_F7,   KC_F8,   KC_NO,   KC_0,    KC_1,    KC_2,   KC_3,    KC_4,    KC_MINS,
+  QWERTY,  KC_F9, KC_F10, KC_F11,  KC_F12,  KC_NO,   KC_5,    KC_6,    KC_7,   KC_8,    KC_9,    KC_NO,
+  GAME_ON, KC_NO, KC_NO,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_NO
 ),
 /* General overlay: numeric keypad, extras.
  * ,-----------------------------------------------------------------------------------.
