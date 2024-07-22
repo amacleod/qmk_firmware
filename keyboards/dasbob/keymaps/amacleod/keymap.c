f768640b9935b42b26d2f7ae1d0ce03a8a2e8622
Allister MacLeod <allister.macleod@gmail.com>
2024-07-22 00:06:30 -0400

    Moving shift and space inward to be more like Corne layout, and filling out nav layer.

--- keyboards/dasbob/keymaps/amacleod/keymap.c
+++ keyboards/dasbob/keymaps/amacleod/keymap.c
@@ -27,6 +27,11 @@ enum dasbob_layers {
 #define COLEMAK TO(_COLEMAK)
 #define QWERTY TO(_QWERTY)
 
+#define SM_LALT OSM(MOD_LALT)
+#define SM_RCTL OSM(MOD_RCTL)
+#define SM_RGUI OSM(MOD_RGUI)
+#define SM_RSFT OSM(MOD_RSFT)
+
 enum combos {
   COMBO_KBDCTL
 };
@@ -49,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 KC_W,    KC_F,    KC_M,    KC_P,    KC_V,        KC_SLSH, KC_DOT,  KC_QUOT, KC_DQT,  KC_J,
 KC_R,    KC_S,    KC_N,    KC_T,    KC_B,        KC_COMM, KC_A,    KC_E,    KC_I,    KC_H,
 KC_X,    KC_C,    KC_L,    KC_D,    KC_G,        KC_MINS, KC_U,    KC_O,    KC_Y,    KC_K,
-                       KC_ESC, KC_LSFT, SYM,   NAV, KC_SPACE, KC_BACKSPACE
+                       KC_ESC, SYM, KC_LSFT,   KC_SPACE, NAV, KC_BACKSPACE
 ),
 /* Dvorak
  * ,-----------------------------.   ,-----------------------------.
@@ -62,7 +67,7 @@ KC_X,    KC_C,    KC_L,    KC_D,    KC_G,        KC_MINS, KC_U,    KC_O,    KC_Y
 KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,        KC_F,    KC_G,    KC_C,    KC_R,    KC_L,
 KC_A,    KC_O,    KC_E,    KC_U,    KC_I,        KC_D,    KC_H,    KC_T,    KC_N,    KC_S,
 KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,        KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,
-                       KC_ESC, KC_LSFT, SYM,   NAV, KC_SPACE, KC_BACKSPACE
+                       KC_ESC, SYM, KC_LSFT,   KC_SPACE, NAV, KC_BACKSPACE
 ),
 /* Colemak-DH
  * ,-----------------------------.   ,-----------------------------.
@@ -75,7 +80,7 @@ KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,        KC_B,    KC_M,    KC_W,    KC_V
 KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,        KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,
 KC_A,    KC_R,    KC_S,    KC_T,    KC_G,        KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
 KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,        KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,
-                       KC_ESC, KC_LSFT, SYM,   NAV, KC_SPACE, KC_BACKSPACE
+                       KC_ESC, SYM, KC_LSFT,   KC_SPACE, NAV, KC_BACKSPACE
 ),
 /* QWERTY
  * ,-----------------------------.   ,-----------------------------.
@@ -88,40 +93,40 @@ KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,        KC_K,    KC_H,    KC_COMM, KC_D
 KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
 KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                       KC_ESC, KC_LSFT, SYM,   NAV, KC_SPACE, KC_BACKSPACE
+                       KC_ESC, SYM, KC_LSFT,   KC_SPACE, NAV, KC_BACKSPACE
 ),
 /* Symbols Layer (LOWER)
  * ,-----------------------------.   ,-----------------------------.
  * |     |     |     |     |     |   |     |     |     |     |     |
  * |     |     |     |     |     |   |     |     |     |     |     |
  * |     |     |     |     |     |   |     |     |     |     |     |
- * `-----------|     |     |#####|   | NUM |     |     |-----------'
+ * `-----------|     |#####|     |   |     | NUM |     |-----------'
  */
 [_LOWER] = LAYOUT_split_3x5_3(
 _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______,
 _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______,
 _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______,
-                    _______, _______, _______,   NUM, _______, _______
+                    _______, _______, _______,   _______,     NUM, _______
 ),
 /* Navigation Layer (RAISE)
  * ,-----------------------------.   ,-----------------------------.
- * |     |     |     |     |     |   |     |     |     |     |     |
- * |     |     |     |     |     |   |     |     |     |     |     |
- * |     |     |     |     |     |   |     |     |     |     |     |
- * `-----------|     |     | NUM |   |#####|     |     |-----------'
+ * |     |     |     |     |     |   | Hom | PgUp|  Up | PgDn| Bks |
+ * |     |     |     |     |     |   | End | Left| Down|Right| Ret |
+ * |     |     |     |     | Del |   | GUI | Ctl |Shift| Alt | Tab |
+ * `-----------|     | NUM |     |   |     |#####| Ins |-----------'
  */
 [_RAISE] = LAYOUT_split_3x5_3(
-_______, _______, _______, _______, _______,       _______, _______, _______, _______, _______,
-_______, _______, _______, _______, _______,       _______, _______, _______, _______, _______,
-_______, _______, _______, _______, _______,       _______, _______, _______, _______, _______,
-                     _______, _______,     NUM,   _______, _______, _______
+_______, _______, _______, _______, _______,       KC_HOME, KC_PGUP, KC_UP,   KC_PGDN, KC_BSPC,
+_______, _______, _______, _______, _______,       KC_END,  KC_LEFT, KC_DOWN, KC_RGHT, KC_ENT,
+_______, _______, _______, _______, KC_DEL,        SM_RGUI, SM_RCTL, SM_RSFT, SM_LALT, KC_TAB,
+                     _______,     NUM, _______,   _______, _______, KC_INS
 ),
 /* Numeric Layer (ADJUST)
  * ,-----------------------------.   ,-----------------------------.
  * |     |     |     |     |     |   |     |     |     |     |     |
  * |     |     |     |     |     |   |     |     |     |     |     |
  * |     |     |     |     |     |   |     |     |     |     |     |
- * `-----------|     |     |#####|   |#####|     |     |-----------'
+ * `-----------|     |#####|     |   |     |#####|     |-----------'
  */
 [_ADJUST] = LAYOUT_split_3x5_3(
 _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______,
