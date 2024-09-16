8c3b7240d8b4d02a15f9dfb9eccd2669f16c6645
Allister MacLeod <allister.macleod@gmail.com>
2024-09-15 20:55:55 -0400

    Switching layer keys further from the middle.
    This should hopefully let me use the reachy middle thumb keys less.

--- keyboards/dasbob/keymaps/amacleod/keymap.c
+++ keyboards/dasbob/keymaps/amacleod/keymap.c
@@ -7,6 +7,7 @@
 #include QMK_KEYBOARD_H
 
 // TO-DO:
+// - rearrange layouts to avoid using stretchiest thumb keys (LH0 and RH0)
 // - numeric layer
 // - symbol layer
 // - hot keys in nav layer
@@ -91,78 +92,78 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |  W  |  F  |  M  |  P  |  V  |   |  /  |  .  |  '  |  "  |  J  |
  * |  R  |  S  |  N  |  T  |  B  |   |  ,  |  A  |  E  |  I  |  H  |
  * |  X  |  C  |  L  |  D  |  G  |   |  -  |  U  |  O  |  Y  |  K  |
- * `-----------| Esc | Shf | SYM |   | NAV | Spc | Bks |-----------'
+ * `-----------| SYM | Shf | Esc |   | Bks | Spc | NAV |-----------'
  */
 [_HDNEU] = LAYOUT_split_3x5_3(
 KC_W,    KC_F,    KC_M,    KC_P,    KC_V,        KC_SLSH, KC_DOT,  KC_QUOT, KC_DQT,  KC_J,
 KC_R,    KC_S,    KC_N,    KC_T,    KC_B,        KC_COMM, KC_A,    KC_E,    KC_I,    KC_H,
 KC_X,    KC_C,    KC_L,    KC_D,    KC_G,        KC_MINS, KC_U,    KC_O,    KC_Y,    KC_K,
-                       KC_ESC, SYM, KC_LSFT,   KC_SPACE, NAV, KC_BACKSPACE
+                      SYM, KC_LSFT, KC_ESC,   KC_BACKSPACE, KC_SPACE, NAV
 ),
 /* Dvorak
  * ,-----------------------------.   ,-----------------------------.
  * |  '  |  ,  |  .  |  P  |  Y  |   |  F  |  G  |  C  |  R  |  L  |
  * |  A  |  O  |  E  |  U  |  I  |   |  D  |  H  |  T  |  N  |  S  |
  * |  ;  |  Q  |  J  |  K  |  X  |   |  B  |  M  |  W  |  V  |  Z  |
- * `-----------| Esc | Shf | SYM |   | NAV | Spc | Bks |-----------'
+ * `-----------| SYM | Shf | Esc |   | Bks | Spc | NAV |-----------'
  */
 [_DVORAK] = LAYOUT_split_3x5_3(
 KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,        KC_F,    KC_G,    KC_C,    KC_R,    KC_L,
 KC_A,    KC_O,    KC_E,    KC_U,    KC_I,        KC_D,    KC_H,    KC_T,    KC_N,    KC_S,
 KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,        KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,
-                       KC_ESC, SYM, KC_LSFT,   KC_SPACE, NAV, KC_BACKSPACE
+                      SYM, KC_LSFT, KC_ESC,   KC_BACKSPACE, KC_SPACE, NAV
 ),
 /* Colemak-DH
  * ,-----------------------------.   ,-----------------------------.
  * |  Q  |  W  |  F  |  P  |  B  |   |  J  |  L  |  U  |  Y  |  '  |
  * |  A  |  R  |  S  |  T  |  G  |   |  M  |  N  |  E  |  I  |  O  |
  * |  Z  |  X  |  C  |  D  |  V  |   |  K  |  H  |  ,  |  .  |  /  |
- * `-----------| Esc | Shf | SYM |   | NAV | Spc | Bks |-----------'
+ * `-----------| SYM | Shf | Esc |   | Bks | Spc | NAV |-----------'
  */
 [_COLEMAK] = LAYOUT_split_3x5_3(
 KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,        KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,
 KC_A,    KC_R,    KC_S,    KC_T,    KC_G,        KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
 KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,        KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,
-                       KC_ESC, SYM, KC_LSFT,   KC_SPACE, NAV, KC_BACKSPACE
+                      SYM, KC_LSFT, KC_ESC,   KC_BACKSPACE, KC_SPACE, NAV
 ),
 /* QWERTY
  * ,-----------------------------.   ,-----------------------------.
  * |  Q  |  W  |  E  |  R  |  T  |   |  Y  |  U  |  I  |  O  |  P  |
  * |  A  |  S  |  D  |  F  |  G  |   |  H  |  J  |  K  |  L  |  ;  |
  * |  Z  |  X  |  C  |  V  |  B  |   |  N  |  M  |  ,  |  .  |  /  |
- * `-----------| Esc | Shf | SYM |   | NAV | Spc | Bks |-----------'
+ * `-----------| SYM | Shf | Esc |   | Bks | Spc | NAV |-----------'
  */
 [_QWERTY] = LAYOUT_split_3x5_3(
 KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
 KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                       KC_ESC, SYM, KC_LSFT,   KC_SPACE, NAV, KC_BACKSPACE
+                      SYM, KC_LSFT, KC_ESC,   KC_BACKSPACE, KC_SPACE, NAV
 ),
 /* Symbols Layer (LOWER)
  * ,-----------------------------.   ,-----------------------------.
  * |  `  |  <  |  >  |  "  |  '  |   |  &  |  :  |  [  |  ]  |  _  |
  * |  !  |  -  |  +  |  =  |  #  |   |  |  |  ;  |  {  |  }  |  ?  |
  * |  ~  |  /  |  *  |  %  |  @  |   |  ^  |  $  |  (  |  )  |  /  |
- * `-----------|     |#####|     |   |     | NUM |  \  |-----------'
+ * `-----------|#####|     |     |   |     |  \  | NUM |-----------'
  */
 [_LOWER] = LAYOUT_split_3x5_3(
 KC_GRV,  KC_LABK, KC_RABK, KC_DQT,  KC_QUOT,     KC_AMPR, KC_COLN, KC_LBRC, KC_RBRC, KC_UNDS,
 KC_EXLM, KC_MINS, KC_PLUS, KC_EQL,  KC_HASH,     KC_PIPE, KC_SCLN, KC_LCBR, KC_RCBR, KC_QUES,
 KC_TILD, KC_SLSH, KC_ASTR, KC_PERC, KC_AT,       KC_CIRC, KC_DLR,  KC_LPRN, KC_RPRN, KC_SLSH,
-                    _______, _______, _______,   _______,     NUM, KC_BSLS
+                    _______, _______, _______,   _______, KC_BSLS, NUM
 ),
 /* Navigation Layer (RAISE)
  * ,-----------------------------.   ,-----------------------------.
  * |     |     |     |     |     |   | Hom | PgUp|  Up | PgDn| Bks |
  * |     |     |     |     |     |   | End | Left| Down|Right| Ret |
  * |     |     |     |     | Del |   | GUI | Ctl |Shift| Alt | Tab |
- * `-----------|     | NUM |     |   |     |#####| Ins |-----------'
+ * `-----------| NUM |     | Ins |   |     |     |#####|-----------'
  */
 [_RAISE] = LAYOUT_split_3x5_3(
 _______, _______, _______, _______, _______,       KC_HOME, KC_PGUP, KC_UP,   KC_PGDN, KC_BSPC,
 _______, _______, _______, _______, _______,       KC_END,  KC_LEFT, KC_DOWN, KC_RGHT, KC_ENT,
 _______, _______, _______, _______, KC_DEL,        SM_RGUI, SM_RCTL, SM_RSFT, SM_LALT, KC_TAB,
-                     _______,     NUM, _______,   _______, _______, KC_INS
+                          NUM, _______, KC_INS,   _______, _______, ______
 ),
 /* Numeric Layer (ADJUST)
  * ,-----------------------------.   ,-----------------------------.
